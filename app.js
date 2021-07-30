64acc72f3bcaa758dccf161431e26cb48b10ec44
Tim Sherry <tsherry@tjs-mbp-2.home>
2021-07-30 13:26:34 -0400

    updated const names

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 //Given the following array of students.  
 const students = [
     {
@@ -26,28 +27,28 @@ const students = [
 //********** Challenge 1 **********************
 ////////Create an array of all the id values.///////
 //example: [1, 2, 3, 4]
-const id = students.map(student => {
+const studentIds = students.map(student => {
     return student.id;
 })
 console.log('**challenge 1**');
-console.log(id);
+console.log(studentIds);
 
 //********** Challenge 2 **********************
 ////////create an array of students 21 and over./////
 //example: [{id: 1,first_name: "John",last_name: "Smith",age: 21}, etc.....]
-const age = students.map(student => {
+const studentAges = students.map(student => {
     if (student.age >= 21) {
         return student;
-    } else return null
-})
+    } return null
+}).filter(student => student !== null)
 console.log('\n**challenge 2**');
-console.log(age);
+console.log(studentAges);
 
 //********** Challenge 3 **********************
 ////////get the first object from the array of students who is older than 15 but younger than 25/////
-const checkAge = students.map(student => {
+const checkAge = students.filter(student => {
     if (student.age > 15 && student.age < 25){
-        return student
+        return student;
     }
 })
 console.log('\n**challenge 3**');
